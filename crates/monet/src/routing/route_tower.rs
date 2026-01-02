e594b99333739907744ee23daa69963cdff61b14
Dawei Hu <hoodavy@gmail.com>
2026-01-02 23:35:27 +0800

    pass build

--- crates/monet/src/routing/route_tower.rs
+++ crates/monet/src/routing/route_tower.rs
@@ -1,8 +1,9 @@
-// use crate::prelude::*;
+use bytes::Bytes;
 use http::Method;
 use pin_project_lite::pin_project;
 use std::{
     convert::Infallible,
+    marker::PhantomData,
     pin::Pin,
     task::{Context, Poll, ready},
 };
@@ -10,14 +11,17 @@ use tower::util::{Oneshot, ServiceExt};
 
 use tower::Service as TowerService;
 
-use crate::{HttpRequest, HttpResponse, IntoResponse};
+use crate::{BoxError, HttpBody, HttpResponse, IntoResponse, opaque_future};
 
-pub struct Route<'r, E = Infallible>(LocalBoxCloneService<'r, HttpRequest, HttpResponse, E>);
+pub struct Body<'a>(Pin<Box<dyn HttpBody<Data = Bytes, Error = BoxError> + 'a>>);
+pub type HttpRequest<'a, T = Body<'a>> = http::Request<T>;
+
+pub struct Route<'r, E = Infallible>(LocalBoxCloneService<'r, HttpRequest<'r>, HttpResponse, E>);
 
 impl<'svc, 'resp, 'fut, E> Route<'svc, E> {
     pub fn new<T>(svc: T) -> Self
     where
-        T: TowerService<HttpRequest, Error = E> + Clone + 'svc,
+        T: TowerService<HttpRequest<'svc>, Error = E> + Clone + 'svc,
         T::Response: IntoResponse + 'resp,
         T::Future: 'fut,
         'svc: 'fut,
@@ -65,6 +69,135 @@ where
         }
     }
 }
+
+// ///  Transfer handler to Route
+// impl<'a, S> BoxedIntoRoute<'a, S, Infallible>
+// where
+//     S: Clone + 'a,
+// {
+//     pub fn from_handler<H, X>(handler: H) -> Self
+//     where
+//         H: Handler<'a, X, S> + 'a,
+//         X: 'a,
+//     {
+//         let svc_fn = |handler, state| {
+//             let svc = HandlerService::new(handler, state);
+//             let resp_map = MapIntoResponse::new(svc);
+//             let lbcs = LocalBoxCloneService::new(resp_map);
+//             Route(lbcs)
+//         };
+//         let erased = ErasedHandler {
+//             handler: handler,
+//             into_route_fn: svc_fn,
+//         };
+//         BoxedIntoRoute(Box::new(erased))
+//     }
+// }
+
+pub trait Handler<'a, X, S>: Clone + Sized + 'a
+where
+    X: 'a,
+    S: 'a,
+    Self: 'a,
+{
+    type Future: Future<Output = HttpResponse> + 'a;
+
+    fn call(self, req: HttpRequest, state: S) -> Self::Future;
+
+    fn to_service(self, state: S) -> HandlerService<Self, X, S> {
+        HandlerService::new(self, state)
+    }
+}
+
+pub struct HandlerService<H, X, S> {
+    pub handler: H,
+    pub state: S,
+    pub(crate) _marker: PhantomData<fn() -> X>,
+}
+
+impl<H, X, S> Clone for HandlerService<H, X, S>
+where
+    H: Clone,
+    S: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            handler: self.handler.clone(),
+            state: self.state.clone(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<H, X, S> HandlerService<H, X, S> {
+    pub(super) fn new(handler: H, state: S) -> Self {
+        Self {
+            handler,
+            state,
+            _marker: PhantomData,
+        }
+    }
+    pub fn state(&self) -> &S {
+        &self.state
+    }
+}
+
+// impl<'a, H, X, S, B> TowerService<HttpRequest<'a, B>> for HandlerService<H, X, S>
+// where
+//     H: Handler<'a, X, S> + Clone + 'a,
+//     B: HttpBody<Data = bytes::Bytes>,
+//     B::Error: Into<BoxError>,
+//     S: Clone + 'a,
+// {
+//     type Response = HttpResponse;
+//
+//     type Error = Infallible;
+//
+//     type Future = IntoServiceFuture<H::Future>;
+//
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         Poll::Ready(Ok(()))
+//     }
+//
+//     fn call(&mut self, req: HttpRequest<B>) -> Self::Future {
+//         use futures_util::future::FutureExt;
+//         let req = req.map(Body::new);
+//         let handler = self.handler.clone();
+//
+//         let future = Handler::call(handler, req, self.state.clone());
+//
+//         let future = future.map(Ok as _);
+//
+//         IntoServiceFuture::new(future)
+//     }
+// }
+//
+//
+// use http_body_util::BodyExt;
+// impl<'a> Body<'a> {
+//     pub fn new<B>(http_body: B) -> Self
+//     where
+//         B: HttpBody<Data = Bytes> + 'a,
+//         B::Error: Into<BoxError>,
+//     {
+//         let body = http_body.map_err(Into::into);
+//         Body(Box::pin(body))
+//     }
+//
+//     pub fn empty() -> Self {
+//         Self::new(http_body_util::Empty::new())
+//     }
+// }
+//
+// use futures::future::Map;
+// opaque_future! {
+//     /// The response future for [`IntoService`](super::IntoService).
+//     pub type IntoServiceFuture<F> =
+//         Map<
+//             F,
+//             fn(HttpResponse) -> Result<HttpResponse, Infallible>,
+//         >;
+// }
 
 pub struct LocalBoxCloneService<'a, T, U, E>(
     Box<
