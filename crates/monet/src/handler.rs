a9b6050211b0578e72fbc719cca1eb0d5c7e5619
Dawei Hu <hoodavy@gmail.com>
2026-01-17 18:07:11 +0800

    cleanup

--- crates/monet/src/handler.rs
+++ crates/monet/src/handler.rs
@@ -16,20 +16,6 @@ pub trait Handler<X>: Clone + Sized + 'static {
     }
 }
 
-// pub trait HandlerWithoutStateExt<T>: Handler<T> {
-//     /// Convert the handler into a [`Service`] and no state.
-//     fn into_service(self) -> HandlerService<Self, T, ()>;
-// }
-//
-// impl<H, T> HandlerWithoutStateExt<T> for H
-// where
-//     H: Handler<T>,
-// {
-//     fn into_service(self) -> HandlerService<Self, T, ()> {
-//         self.to_service(())
-//     }
-// }
-
 impl<F, Fut, Res> Handler<((),)> for F
 where
     F: FnOnce() -> Fut + Clone + 'static,
@@ -87,7 +73,7 @@ macro_rules! all_the_tuples {
     ($name:ident) => {
         $name!([], T1);
         $name!([T1], T2);
-        // $name!([T1, T2], T3);
+        $name!([T1, T2], T3);
         $name!([T1, T2, T3], T4);
         $name!([T1, T2, T3, T4], T5);
         $name!([T1, T2, T3, T4, T5], T6);
@@ -102,38 +88,6 @@ macro_rules! all_the_tuples {
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14], T15);
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15], T16);
     };
-}
-
-#[allow(non_snake_case, unused_mut)]
-impl<F, Fut, Res, M, T1, T2, T3> Handler<(M, T1, T2, T3)> for F
-where
-    F: FnOnce(T1, T2, T3) -> Fut + Clone + 'static,
-    Fut: Future<Output = Res>,
-    Res: IntoResponse,
-    T1: FromRequestParts,
-    T2: FromRequestParts,
-    T3: FromRequest<M>,
-{
-    type Future = Pin<Box<dyn Future<Output = HttpResponse>>>;
-    fn call(self, req: HttpRequest) -> Self::Future {
-        let (mut parts, body) = req.into_parts();
-        Box::pin(async move {
-            let T1 = match T1::from_request_parts(&mut parts).await {
-                Ok(value) => value,
-                Err(rejection) => return rejection.into_response(),
-            };
-            let T2 = match T2::from_request_parts(&mut parts).await {
-                Ok(value) => value,
-                Err(rejection) => return rejection.into_response(),
-            };
-            let req = HttpRequest::from_parts(parts, body);
-            let T3 = match T3::from_request(req).await {
-                Ok(value) => value,
-                Err(rejection) => return rejection.into_response(),
-            };
-            self(T1, T2, T3).await.into_response()
-        })
-    }
 }
 
 all_the_tuples!(impl_handler);
