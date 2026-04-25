dfe110fee4af7239b1d9475a161ce640242ada6f
Dawei Hu <hoodavy@gmail.com>
2026-04-25 17:37:07 +0800

    update

--- examples/hello-compio/src/main.rs
+++ examples/hello-compio/src/main.rs
@@ -1,5 +1,9 @@
+use std::net::SocketAddr;
+
 use monet::serve::serve;
 
 fn main() {
-    println!("Hello, world!");
+    let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
+    println!("Running http server on {}", addr);
+    serve(addr);
 }
