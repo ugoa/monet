051fad5b9c0b83e519080ce77d5387eff0a8e42a
Dawei Hu <hoodavy@gmail.com>
2026-05-08 18:27:11 +0800

    add body

--- crates/monet/src/body.rs
+++ crates/monet/src/body.rs
@@ -1,9 +1,42 @@
 use bytes::Bytes;
+use http_body_util::BodyExt;
 
-use crate::error::Error;
+use crate::error::{BoxError, Error};
+
+type BoxBody = http_body_util::combinators::UnsyncBoxBody<Bytes, Error>;
 
 #[must_use]
 #[derive(Debug)]
 pub struct Body(BoxBody);
 
-type BoxBody = http_body_util::combinators::UnsyncBoxBody<Bytes, Error>;
+impl Body {
+    /// Create a new `Body` that wraps another [`http_body::Body`].
+    pub fn new<B>(body: B) -> Self
+    where
+        B: http_body::Body<Data = Bytes> + Send + 'static,
+        B::Error: Into<BoxError>,
+    {
+        try_downcast(body).unwrap_or_else(|body| Self(boxed(body)))
+    }
+}
+
+fn boxed<B>(body: B) -> BoxBody
+where
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<BoxError>,
+{
+    try_downcast(body).unwrap_or_else(|body| body.map_err(Error::new).boxed_unsync())
+}
+
+pub(crate) fn try_downcast<T, K>(k: K) -> Result<T, K>
+where
+    T: 'static,
+    K: Send + 'static,
+{
+    let mut k = Some(k);
+    if let Some(k) = <dyn std::any::Any>::downcast_mut::<Option<T>>(&mut k) {
+        Ok(k.take().unwrap())
+    } else {
+        Err(k.unwrap())
+    }
+}
