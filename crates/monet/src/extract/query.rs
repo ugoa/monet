fccdeced67df6d2e4fada7e2bb3e432973b8465a
Dawei Hu <hoodavy@gmail.com>
2026-05-09 19:34:48 +0800

    add deref for query

--- crates/monet/src/extract/query.rs
+++ crates/monet/src/extract/query.rs
@@ -1,2 +1,16 @@
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Query<T>(pub T);
+
+impl<T> std::ops::Deref for Query<T> {
+    type Target = T;
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl<T> std::ops::DerefMut for Query<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
