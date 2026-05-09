cac66db07419190cd52f3a3f1303a839b9efe427
Dawei Hu <hoodavy@gmail.com>
2026-05-09 20:10:51 +0800

    add

--- crates/monet/src/form.rs
+++ crates/monet/src/form.rs
@@ -1,3 +1,17 @@
 #[derive(Debug, Clone, Copy, Default)]
 #[must_use]
 pub struct Form<T>(pub T);
+
+impl<T> std::ops::Deref for Form<T> {
+    type Target = T;
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl<T> std::ops::DerefMut for Form<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
