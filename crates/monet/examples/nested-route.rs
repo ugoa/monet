1c09e6726feb446a4527c3ede9392a14743d727d
Dawei Hu <hoodavy@gmail.com>
2026-05-12 12:35:18 +0800

    add more test

--- crates/monet/examples/nested-route.rs
+++ crates/monet/examples/nested-route.rs
@@ -3,20 +3,28 @@ use std::net::SocketAddr;
 use monet::{Request, Router, get};
 
 async fn hello(_req: Request) -> String {
-    "Hello from /api/hello".to_string()
+    "Hello from /public/hello".to_string()
 }
 
 async fn hi(_req: Request) -> String {
-    "Hello from /api/hi".to_string()
+    "Hello from /public/hi".to_string()
+}
+
+async fn private(_req: Request) -> String {
+    "Hello from /private".to_string()
 }
 
 fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     println!("Server running at: {}", addr);
 
-    let nested = Router::new().at("/hello", get(hello)).at("/hi", get(hi));
+    let public = Router::new().at("/hello", get(hello)).at("/hi", get(hi));
 
-    let app = Router::new().nest("/api", nested);
+    let private = Router::new().at("/secret/{*rest}", get(private));
+
+    let app = Router::new()
+        .nest("/public", public)
+        .nest("/private", private);
 
     monet::run(addr, app);
 }
