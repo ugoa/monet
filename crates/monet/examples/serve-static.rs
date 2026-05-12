ad4cea4ccf3fdb17cfe1c774a8762edb930db8b0
Dawei Hu <hoodavy@gmail.com>
2026-05-12 14:24:46 +0800

    reorg

--- crates/monet/examples/serve-static.rs
+++ crates/monet/examples/serve-static.rs
@@ -1,12 +1,12 @@
 use std::net::SocketAddr;
 
-use monet::{Router, ServeDir};
+use monet::Router;
 
 fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     println!("Server running at: {}", addr);
 
-    let app = Router::new().serve_dir("/assets", "static/assets");
+    let app = Router::new().serve_dir("/assets", "./crates/monet/examples/assets");
 
     monet::run(addr, app);
 }
