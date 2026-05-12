f18b4d71ff863235f993a7abe690f40f3bbb038c
Dawei Hu <hoodavy@gmail.com>
2026-05-12 20:50:07 +0800

    add matched path

--- crates/monet/examples/wildcard-route.rs
+++ crates/monet/examples/wildcard-route.rs
@@ -7,11 +7,19 @@ async fn catch_all(req: Request) -> String {
     format!("nothing excapes by matcher: {matched}")
 }
 
+async fn hello(req: Request) -> String {
+    let matched = req.matched_path().unwrap();
+    format!("matched nested path: {matched}")
+}
+
 fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     println!("Server running at: {}", addr);
 
-    let app = Router::new().at("/wild/{id}/card/{*another}", get(catch_all));
+    let nested = Router::new().at("/hello/{*rest}", get(hello));
+    let app = Router::new()
+        .at("/wild/{id}/card/{*another}", get(catch_all))
+        .nest("/api", nested);
 
     monet::run(addr, app);
 }
