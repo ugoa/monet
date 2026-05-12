66f77f6a7814f68c753e8254078e857f8f6e5373
Dawei Hu <hoodavy@gmail.com>
2026-05-12 14:22:47 +0800

    finish serve dir

--- crates/monet/examples/serve-static.rs
+++ crates/monet/examples/serve-static.rs
@@ -1,12 +1,12 @@
 use std::net::SocketAddr;
 
-use monet::{Router, ServeDir, service};
+use monet::{Router, ServeDir};
 
 fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     println!("Server running at: {}", addr);
 
-    let app = Router::new().at("/hello.html", service(ServeDir::new("static")));
+    let app = Router::new().serve_dir("/assets", "static/assets");
 
     monet::run(addr, app);
 }
