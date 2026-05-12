067d8150e9421a0999dbe82d81f4d95953bab18a
Dawei Hu <hoodavy@gmail.com>
2026-05-12 14:34:39 +0800

    add

--- crates/monet/examples/serve-static.rs
+++ crates/monet/examples/serve-static.rs
@@ -2,6 +2,8 @@ use std::net::SocketAddr;
 
 use monet::Router;
 
+/// To Succeed, please run this one from monet project root.
+/// cargo run --examples serve-static
 fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     println!("Server running at: {}", addr);
