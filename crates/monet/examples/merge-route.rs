c5983868a919f3810cd5015fbbd3fbeba654b83b
Dawei Hu <hoodavy@gmail.com>
2026-05-13 17:15:50 +0800

    add

--- crates/monet/examples/merge-route.rs
+++ crates/monet/examples/merge-route.rs
@@ -14,5 +14,7 @@ fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
     let app5 = Router::new().at("/hi", get(hi));
     let app6 = Router::new().at("/hello", post(hello));
+
+    println!("Server running at: {}", addr);
     monet::run(addr, app5.merge(app6));
 }
