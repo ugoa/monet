98813ef1ff9a90870523e31e2b9a73dccfc7e56a
Dawei Hu <hoodavy@gmail.com>
2026-05-13 17:09:57 +0800

    add test

---
+++ crates/monet/examples/merge-route.rs
@@ -0,0 +1,18 @@
+use std::net::SocketAddr;
+
+use monet::{Request, Router, get, post};
+
+async fn hello(_req: Request) -> &'static str {
+    "hello"
+}
+
+async fn hi(_req: Request) -> &'static str {
+    "hello"
+}
+
+fn main() {
+    let addr: SocketAddr = ([0, 0, 0, 0], 9527).into();
+    let app5 = Router::new().at("/hi", get(hi));
+    let app6 = Router::new().at("/hello", post(hello));
+    monet::run(addr, app5.merge(app6));
+}
